7ec2ae0cdc6d4dd60768e2aff08a959dc489e316
Naika Jith <naika.jith@outlook.com>
2026-06-03 20:47:45 -0400

    Almost done intro!

--- ICS3U Final Project-European Roulette/Game1.cs
+++ ICS3U Final Project-European Roulette/Game1.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.VisualBasic.FileIO;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -7,17 +6,34 @@ namespace ICS3U_Final_Project_European_Roulette
 {
     public class Game1 : Game
     {
+        enum Screen
+        {
+            Intro,
+            Letter
+        }
+
+        Screen currentScreen;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
         Rectangle window;
 
+        // Textures
         Texture2D introTexture;
+        Texture2D letterTexture;
 
         Rectangle introRect;
+        Rectangle letterRect;
+
+        // Buttons
+        Texture2D beginthenightTexture;
+        Rectangle beginthenightRect;
+
+        Texture2D stepoutsideTexture;
+        Rectangle stepoutsideRect;
 
         MouseState mouseState;
-
 
         public Game1()
         {
@@ -28,14 +44,19 @@ namespace ICS3U_Final_Project_European_Roulette
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             window = new Rectangle(0, 0, 950, 600);
+
             _graphics.PreferredBackBufferWidth = window.Width;
             _graphics.PreferredBackBufferHeight = window.Height;
             _graphics.ApplyChanges();
 
+            currentScreen = Screen.Intro;
+
             introRect = new Rectangle(0, 0, 950, 600);
+            letterRect = new Rectangle(0, 0, 950, 600);
+
+            beginthenightRect = new Rectangle(600, 120, 325, 115);
+            stepoutsideRect = new Rectangle(600, 260, 325, 115);
 
             base.Initialize();
         }
@@ -44,43 +65,60 @@ namespace ICS3U_Final_Project_European_Roulette
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            introTexture = Content.Load<Texture2D>("intro");
+            letterTexture = Content.Load<Texture2D>("letter");
 
-            
+            beginthenightTexture = Content.Load<Texture2D>("begin the night");
+            stepoutsideTexture = Content.Load<Texture2D>("step outside");
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
-
             mouseState = Mouse.GetState();
 
-            this.Window.Title = mouseState.Position.ToString();
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
 
-            introTexture = Content.Load<Texture2D>("intro");
+           
+            if (currentScreen == Screen.Intro)
+            {
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                {
+                  
+                    if (beginthenightRect.Contains(mouseState.Position))
+                    {
+                        currentScreen = Screen.Letter;
+                    }
 
-
-
+                   
+                    if (stepoutsideRect.Contains(mouseState.Position))
+                    {
+                        Exit();
+                    }
+                }
+            }
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.SeaGreen);
-
-            // TODO: Add your drawing code here
+            GraphicsDevice.Clear(Color.Black);
 
             _spriteBatch.Begin();
 
-            _spriteBatch.Draw(introTexture, introRect, Color.White);
+            if (currentScreen == Screen.Intro)
+            {
+                _spriteBatch.Draw(introTexture, introRect, Color.White);
+                _spriteBatch.Draw(beginthenightTexture, beginthenightRect, Color.White);
+                _spriteBatch.Draw(stepoutsideTexture, stepoutsideRect, Color.White);
+            }
+            else if (currentScreen == Screen.Letter)
+            {
+                _spriteBatch.Draw(letterTexture, letterRect, Color.White);
+            }
 
             _spriteBatch.End();
-
-
 
             base.Draw(gameTime);
         }
